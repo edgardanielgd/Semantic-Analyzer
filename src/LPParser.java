77a7fd54fc7812d8aa66104484123eb9f0cab235
edgardanielgd <edgardanielgd123@gmail.com>
2023-05-03 23:40:18 -0500

    Added while and ended if support, adding variables to contexts is pending

---
+++ src/LPParser.java
@@ -0,0 +1,4 @@
+package src;
+import gen.LPGrammarParser;
+
+
--- src/LPVisitor.java
+++ src/LPVisitor.java
@@ -3,6 +3,7 @@ package src;
 import java.util.*;
 
 import gen.*;
+import org.antlr.v4.runtime.ParserRuleContext;
 import src.CommonTypes;
 
 public class LPVisitor<T> extends LPGrammarBaseVisitor<T> {
@@ -131,12 +132,12 @@ public class LPVisitor<T> extends LPGrammarBaseVisitor<T> {
         // Decrease indentation since embeded code has already been parsed
         this.indentationLevel--;
 
+        // End if corpus
+        this.output += getIndentedLine("}", false);
+
         // Visit if continuation
         // (which at this point has the same indentation level than original if)
         visit(ctx.ifcontinuation());
-
-        // End if corpus
-        this.output += getIndentedLine("}", true);
 
         return null;
     }
@@ -147,11 +148,157 @@ public class LPVisitor<T> extends LPGrammarBaseVisitor<T> {
         // 1. EndIf, just end the conditional body
         // 2. ElseIf, read another conditional
         // 3. Else, final block of conditional
-        System.out.println("Pasa");
-        Integer alternative = ctx.getRuleIndex();
-        System.out.println(alternative);
+        // Note for this point a new line hasn't been printed yet
+        System.out.println("Translates if continuation....");
+        if( ctx.ELSE() != null ){
+            // This the "else statement endif" case
+            this.output += " else {\n"; // No indent needed (added in parent rule)
+
+            // Create a block for else statement
+            this.indentationLevel++;
+            this.output += getIndentedLine(
+                    "/* Else body */", true
+            );
+
+            if(ctx.statement() != null)
+                visit(ctx.statement());
+
+            // Rollback indentation since else is finished
+            this.indentationLevel--;
+
+            // Close else brackets
+            this.output += getIndentedLine("}", true);
+        } else if(ctx.ELSEIF() != null){
+            // This is the "elseif ( expression ) then statement ifcontinuation" case
+            this.output += " else if ( "; // No indent needed (added in parent rule)
+
+            // Visit child expression (which represents a conditional)
+            // Note expressions won't print new lines by default
+            visit(ctx.expression());
+
+            this.output += ") {\n"; // No indent needed
+
+            // Create a block for else if statement
+            this.indentationLevel++;
+            this.output += getIndentedLine(
+                    "/* Else if body */", true
+            );
+
+            if(ctx.statement() != null)
+                visit(ctx.statement());
+
+            // Rollback indentation since else if is finished
+            this.indentationLevel--;
+
+            // Close else if brackets (new line will be decided by next rule)
+            this.output += getIndentedLine("}", false);
+
+            // Visit if continuation
+            // (which at this point has the same indentation level than original if)
+            visit(ctx.ifcontinuation());
+        } else if (ctx.ENDIF() != null){
+            // This is the "endif" case
+            // Note there is a pending new line to be printed (Next to last { token)
+            this.output += getIndentedLine(
+                    "\n/* End of if */", true
+            );
+        }
+
         return null;
     }
+
+    @Override
+    public T visitWhiledeclaration(LPGrammarParser.WhiledeclarationContext ctx){
+        // Visit a while declaration
+        // Resulting code will be defined as:
+        // while( <condition> ) { <body> }
+        // indentation level will be considered
+        System.out.println("Reading while declaration...");
+        this.output += getIndentedLine("// While declaration", true) +
+                getIndentedLine(
+                        "while ( "
+                        , false);
+
+        // Visit child expression (which represents a conditional)
+        // Note expressions won't print new lines by default
+        visit(ctx.expression());
+
+        this.output += ") {\n"; // No indent needed
+
+        // Increase indentation level for embeded code
+        this.indentationLevel++;
+
+        this.output += getIndentedLine(
+                "/* While body */", true
+        );
+
+        // Print related statement data indented
+        if(ctx.statement() != null)
+            visit(ctx.statement());
+
+        // Decrease indentation since embeded code has already been parsed
+        this.indentationLevel--;
+
+        // End while corpus
+        this.output += getIndentedLine("}", true);
+
+        return null;
+    }
+
+    @Override
+    public T visitFordeclaration(LPGrammarParser.FordeclarationContext ctx){
+        // Visit a for declaration
+        // Resulting code will be defined as:
+        // for( <assignment> ; <condition> ; <assignment> ) { <body> }
+        // indentation level will be considered
+        System.out.println("Reading for declaration...");
+        this.output += getIndentedLine("// For declaration", true) +
+                getIndentedLine(
+                        "for ( "
+                        , false);
+
+        // Check if variable is already defined, if not, define it locally in
+        // target code
+        String iteratorName = ctx.IDENTIFIER().getText();
+        CommonTypes iterator = this.variables.get(iteratorName);
+
+
+        if( iterator == null ){
+            // Iterator variable hasn't been defined at this point, better to define it
+            // locally in target code
+            this.output += "let ";
+        }
+
+        this.output += iteratorName + " = ";
+
+        // TODO: Check iterator is an integer variable
+        // How the heck to do that?
+
+        // Generate first expression value (first assignation for iterator)
+        visit(ctx.expression(0));
+
+        this.output += "; "; // No indent needed
+
+        // Now generate stop condition, it will always have the form:
+        // <iterator> <= <expression>
+        this.output += iteratorName + " <= ";
+
+        // TODO: Check again if iterator is an integer variable
+        // How the heck to do that? x2
+        visit(ctx.expression(1));
+
+        this.output += "; "; // No indent needed
+
+        // Now, check if there is a STEP keyword or we can deduce its just + 1
+        // This will be handled by next rule: fordeclarationcomp
+        visit(ctx.fordeclarationcomp());
+
+        return null;
+    }
+    // TODO: Complete forcomp visitor
+    // Recall that we need a custom context attribute there for passing in the
+    // iteration varname
+
     // Useful function for printing with indentation certain block codes
     public String getIndentedLine(String line, Boolean newLine){
         // sometimes we may not add a new line, for example, within If conditions
